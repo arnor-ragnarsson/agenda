3a865c9532e9dc06dcee2704d7d46c9638d4e9a7
Arnór Ragnarsson <arnor.ragnarsson13@gmail.com>
2016-11-01 11:46:32 +0000

    allt að verða klárt!

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
   include('inc/header.php');
   // This page is your welcome page. here will be info about our task management system and also login and signup availability.
 ?>
-
+<h1>Agenda</h1>
 <?php loginNav($navItems); ?>
 
 <?php   include('inc/footer.php'); ?>
